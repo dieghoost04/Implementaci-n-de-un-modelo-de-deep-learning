898c7beea58ea43b17672676cfc3919a52474ba1
dieghoost04 <105373605+dieghoost04@users.noreply.github.com>
2023-11-26 23:54:52 -0600

    Add files via upload

--- model_deep_learning.py
+++ model_deep_learning.py
@@ -17,12 +17,13 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
 import os
+from PIL import Image
 import numpy as np
 
-directory = './chest_xray'
-directorio_train = './chest_xray/train'
-directorio_test = './chest_xray/test'
-directorio_val = './chest_xray/val'
+directory = './images'
+directorio_train = './images/entrenamiento'
+directorio_test = './images/prueba'
+directorio_val = './images/validacion'
 
 
 train_dir = os.path.join(directorio_train)
@@ -118,9 +119,9 @@ class cnn:
         self.model.add(base_model)
         self.model.add(GlobalAveragePooling2D())
         self.model.add(Dropout(0.5))
-        self.model.add(layers.Dense(1664,activation='selu'))
-        self.model.add(layers.Dense(1,activation='sigmoid'))
-        self.model.compile(optimizer=Adam(learning_rate=learning_rate), loss=loss, metrics=[metrics])
+        self.model.add(layers.Dense(512, activation='selu'))  # Reducción del número de neuronas
+        self.model.add(layers.Dense(1, activation='sigmoid'))  # Activación 'sigmoid' para clasificación binaria
+        self.model.compile(optimizer=Adam(learning_rate=learning_rate), loss='binary_crossentropy', metrics=['accuracy'])
 
         self.model.summary()
 
@@ -192,37 +193,95 @@ class cnn:
         print('\n EVALUATION \n')
 
         # Se cargan los mejores pesos que se guardan del entrenamiento
-        self.model.load_weights('best_model.h5')
+        #self.model.load_weights('best_model.h5')
         test_datagen = ImageDataGenerator(1./255)
 
-        test_generator = test_datagen.flow_from_directory(
+        self.test_generator = test_datagen.flow_from_directory(
                             self.test_dir,
                             target_size = (224, 224),
                             batch_size = batch_size,
                             class_mode= 'binary')
 
-        test_loss, test_acc = self.model.evaluate(test_generator)
-        class_names = list(test_generator.class_indices.keys())
+        test_loss, test_acc = self.model.evaluate(self.test_generator)
+        self.class_names = list(self.test_generator.class_indices.keys())
 
         print('\nTest accuracy :\n', test_acc)
         print('\nTest loss :\n', test_loss)
 
-        # Realiza predicciones en el conjunto de prueba
-        y_pred = self.model.predict(test_generator)
+        self.y_pred = self.model.predict(self.test_generator)
 
-        # Convierte las probabilidades en etiquetas de clase
-        y_pred_labels = np.argmax(y_pred, axis=1)
+        self.y_pred_labels = (self.y_pred > 0.5).astype(int)
 
-        # Las etiquetas reales ya están en test_generator.labels
-        test_labels = test_generator.labels
+        self.test_labels = self.test_generator.labels
 
-        confusion = confusion_matrix(test_labels, y_pred_labels)
+        confusion = confusion_matrix(self.test_labels, self.y_pred_labels)
 
         # Muestra la matriz de confusión
-        disp = ConfusionMatrixDisplay(confusion, display_labels = class_names)  # Reemplaza "classes" con tus nombres de clases
+        disp = ConfusionMatrixDisplay(confusion, display_labels = self.class_names)  # Reemplaza "classes" con tus nombres de clases
         disp.plot(cmap='Blues', values_format='d')
         plt.title('Matriz de Confusión')
         plt.show()
+
+    def prediction(self):
+        '''
+        Este método hace predicciones de imágenes aleatorias, y muestra la predicción y la clase real.
+        '''
+        while True:
+            # Selecciona una imagen del conjunto de prueba
+            image_index = np.random.randint(0, len(self.test_labels))  # Escoge una imagen aleatoria
+
+            # Obtiene la ruta de la imagen
+            image_path = self.test_generator.filepaths[image_index]
+
+            # Carga la imagen
+            image = plt.imread(image_path)
+
+            # Realiza la predicción en la imagen seleccionada
+            predicted_class = int(self.y_pred_labels[image_index])
+            real_class = self.test_labels[image_index]
+
+            # Muestra la imagen
+            plt.imshow(image, cmap='gray')
+            plt.title(f'Predicción: {self.class_names[predicted_class]}, Clase Real: {self.class_names[real_class]}')
+            plt.show()
+
+    def prediction_internet(self):
+        '''
+        Este método hace predicciones de imágenes aleatorias, y muestra la predicción y la clase real.
+        '''
+        image_path = './imagen.jpg'  # Reemplaza con la ruta correcta a tu imagen
+        
+        image = Image.open(image_path)
+
+        # Asegúrate de que la imagen tenga las dimensiones correctas y se preprocese según sea necesario
+        # Aquí, se asume que el modelo originalmente fue entrenado con imágenes de tamaño (224, 224, 3)
+        # Si tu modelo espera un tamaño diferente, ajusta el tamaño y el preprocesamiento en consecuencia
+
+        # Ajusta el tamaño a (224, 224)
+        image = image.resize((224, 224))
+        
+        # Convierte la imagen a un array NumPy
+        image_array = np.array(image)
+
+        # Normaliza los valores de píxeles
+        image_array = image_array / 255.0
+
+        # Agrega una dimensión adicional para la dimensión del lote (batch)
+        image_array = np.expand_dims(image_array, axis=0)
+
+        # Realiza la predicción en la imagen seleccionada
+        predicted_prob = self.model.predict(image_array)[0][0]
+        predicted_class = int(predicted_prob > 0.5)  # Usando un umbral de 0.5 para clasificación binaria
+
+        # Muestra la imagen
+        plt.imshow(image_array[0], cmap='gray')
+        plt.title(f'Predicción: {self.class_names[predicted_class]}, Probabilidad: {predicted_prob:.4f}')
+        plt.show()
+
+    # Ejemplo de uso
+    
+    
+
 
 
 def evaluate():
@@ -231,6 +290,7 @@ def evaluate():
     
     xray = cnn(train_dir, test_dir, val_dir)
     xray.evaluation()
+    xray.prediction_internet()
 
 def train():
     '''Para entrenar el modelo, poder visualizar los cambios que se hicieron
@@ -240,6 +300,7 @@ def train():
     xray.plot_images()
     xray.model_fit()
     xray.plot_train_parameters()
+    
 
 
 evaluate()
